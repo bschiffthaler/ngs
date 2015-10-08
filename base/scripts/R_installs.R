897dec9d16a7a671d25a5cf339b2454a2f1b14df
Bastian Schiffthaler <bastian@bioinformatics.upsc.se>
2015-10-08 13:38:38 +0200

    Cleared out redundancy in the R packages. Jupyter now starts in the home dir of the training user

--- base/scripts/R_installs.R
+++ base/scripts/R_installs.R
@@ -2,6 +2,16 @@ rlib='/home/training/.r-library'
 install.packages(c('rzmq','repr','IRkernel','IRdisplay'),
                  repos = c('http://irkernel.github.io/', getOption('repos')),
                  type = 'source', lib = rlib)
-.libPaths()
 IRkernel::installspec()
+
+library(devtools)
+install_github('catavallejos/BASiCS', lib = rlib)
+
+library(BiocInstaller)
+biocLite(c('pvclust', 'vsn'),
+         ask = FALSE,
+         suppressUpdates = TRUE,
+         suppressAutoUpdate = TRUE,
+         lib = rlib)
+
 
